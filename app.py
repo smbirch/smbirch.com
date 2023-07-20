7153149612a480c7b18541e567ace7db3bd99197
smbirch <spencer.m.birch@gmail.com>
2023-07-19 20:04:37 -0700

    adding basic logging

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import logging
+
+logger = logging.getLogger("smbirchcom")
+logging.basicConfig(
+    filename="logs.log",
+    level=logging.DEBUG,
+    format=f"%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    datefmt="%m/%d/%Y %I:%M:%S %p",
+)
 
 app = Flask(__name__)
 
 
 @app.route("/", methods=["GET"])
 def index():
+    ipaddr = request.environ.get("HTTP_X_REAL_IP", request.remote_addr)
+    logger.info(f"homepage : {ipaddr}")
     return render_template("index.html")
 
 
