01a0ccd65dd542d9a6757988011a7d00e5d9c8cf
smbirch <spencer.m.birch@gmail.com>
2023-07-21 22:55:25 -0700

    adding tests and "about" page

--- tests/test_app.py
+++ tests/test_app.py
@@ -11,10 +11,19 @@ def client():
     def index():
         return "Hello, World!"
 
+    @app.route("/about", methods=["GET"])
+    def index():
+        return "Hello, World!"
+
     with app.test_client() as client:
         yield client
 
 
 def test_index_page(client):
     response = client.get("/")
+    assert response.status_code == 200
+
+
+def test_about_page(client):
+    response = client.get("/about")
     assert response.status_code == 200
